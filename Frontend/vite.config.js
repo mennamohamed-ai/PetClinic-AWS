a88f0c15e7c92dce3adf16f47ca7a9cd87e4cc7e
Menna Mohamed <168407693+mennamohamed-ai@users.noreply.github.com>
2026-05-11 18:43:33 +0300

    .

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    define: {
+      
+      'process.env.REACT_APP_API_URL': JSON.stringify(env.VITE_APP_API_URL || 'http://petclinic-prod-alb-2142133629.us-east-1.elb.amazonaws.com'),
+      'import.meta.env.VITE_APP_API_URL': JSON.stringify(env.VITE_APP_API_URL || 'http://petclinic-prod-alb-2142133629.us-east-1.elb.amazonaws.com')
+    }
+  }
 })
